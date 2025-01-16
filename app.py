6a3018eb5788882b6cf952631a77ffc1f19e5c38
Shakeel-ai <146809696+Shakeel-ai@users.noreply.github.com>
2025-01-16 19:24:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from langchain_openai import ChatOpenAI
 
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title="shakeel")
 st.title("YouTube Video Summarizer")
 
